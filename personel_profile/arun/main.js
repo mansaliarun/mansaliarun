1a1415e427fd9cda754b1b7dad3367e61bfc195b
M Arun Kumar <141622060+mansaliarun@users.noreply.github.com>
2025-06-13 10:48:29 +0530

    Add files via upload

---
+++ personel_profile/arun/main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text",{
+    strings:["Python Developer", "Frontend Developer", "Business system"],
+    typeSpeed : 10,
+    backSpeed : 100,
+    backDelay : 1000,
+    loop : true
+});
